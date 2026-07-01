63bf62fed91a43d8e5552d0be75a9a1d5fdc7af0
hasup.lee <hasup.lee@samsung.com>
2026-07-01 14:41:41 +0900

    [refactoring] 1

--- test_baseball.py
+++ test_baseball.py
@@ -1,7 +1,11 @@
 from baseball import Game
 import pytest
 
-def test_exception_when_input_is_none():
+@pytest.fixture
+def game():
+    return Game()
+
+def test_exception_when_input_is_none(game):
     game = Game()
     with pytest.raises(TypeError):
         game.guess(None)
