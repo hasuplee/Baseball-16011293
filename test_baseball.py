5aa16bf8a608dab90d5ecc60c70b97bd98edc256
hasup.lee <hasup.lee@samsung.com>
2026-07-01 14:52:23 +0900

    [feature] 4

--- test_baseball.py
+++ test_baseball.py
@@ -20,3 +20,6 @@ def test_exception_when_invalid_input(game):
     assert_illegal_argument(game, None)
     assert_illegal_argument(game, "1234")
     assert_illegal_argument(game, "12s")
+    assert_illegal_argument(game, "121")
+
+
